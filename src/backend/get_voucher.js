089f350b5cc86133db537c839869345526b6f2d9
Nechama Geldzahler <ng@jointtechs.com>
2026-07-21 18:08:12 +0300

    display change

--- src/backend/get_voucher.js
+++ src/backend/get_voucher.js
@@ -4,7 +4,7 @@ if (!voucher_code) throw new Error("Voucher Code is required!");
 
 var rows = await callReport(4, { voucher_code, user_id: envData.user.id });
 
-if (!rows || !rows.length) throw new Error("Voucher Not Found!");
+if (!rows || !rows.length) throw new Error("Voucher Not Found");
 
 let voucher = rows[0];
 return voucher;
